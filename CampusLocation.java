22a44e3c85dd76bacd5dfc6c20037cb190b31c7b
22ug3-0719-cpu <22ug3-0719@sltc.ac.lk>
2026-09-26 00:10:26 +0530

    Add files via upload

---
+++ CampusLocation.java
@@ -0,0 +1,17 @@
+public class CampusLocation {
+
+    private String name;
+
+    public CampusLocation(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
